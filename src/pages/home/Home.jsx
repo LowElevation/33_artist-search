503747bb6902034880c3e3f3bf4cd097537ff282
dl-watson <dlarkinwatson@gmail.com>
2021-02-11 11:25:51 -0800

    tested all fetch functions in Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,20 @@
 import React, { useState, useEffect } from "react";
+import albumsByID from "../../utils/albumsByID";
 import artistsByName from "../../utils/artistsByName";
+import lyricsByArtistAndTitle from "../../utils/lyricsByArtistAndTitle";
+import tracklistByAlbumID from "../../utils/tracklistByAlbumID";
 
+// https://www.youtube.com/watch?v=WK4HHaNhcgU
 const Home = () => {
   // const [search, useSearch] = useState(null);
 
+  const artistName = "Barbra Streisand";
+  const artistID = "56cd15a1-0d74-438b-8244-c96ffe1cae03";
+  const albumID = "064bcb0c-2eaf-4e4a-a83b-cb53d616dbbd";
+  const songTitle = "Down With Love";
+
   useEffect(() => {
-    return artistsByName("barbara streisand").then(console.log);
+    return lyricsByArtistAndTitle(artistName, songTitle).then(console.log);
   }, []);
 
   return <>Home</>;
--- src/utils/lyricsByArtistAndTitle.js
+++ src/utils/lyricsByArtistAndTitle.js
@@ -1,2 +1,10 @@
 // https://lyricsovh.docs.apiary.io/#reference/0/lyrics-of-a-song/search?console=1
 // https://api.lyrics.ovh/v1/madonna/how%20high
+
+const lyricsByArtistAndTitle = (artist, title) => {
+  return fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`).then((res) =>
+    res.json()
+  );
+};
+
+export default lyricsByArtistAndTitle;
