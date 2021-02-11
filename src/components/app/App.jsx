c22f858363d99617393689850f0458023067c339
dl-watson <dlarkinwatson@gmail.com>
2021-02-11 10:55:56 -0800

    set up Router, confirmed that imports are working as expected

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "../../pages/home/Home";
+import ArtistSearch from "../../pages/search/ArtistSearch";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/search" component={ArtistSearch} />
+          <Route path="/releases/:artistID/" component="" />
+          <Route path="/releases/tracklist/:albumID" component="" />
+          <Route path="/releases/lyrics/:songID" component="" />
+        </Switch>
+      </Router>
+    </>
+  );
 };
-
 export default App;
---
+++ src/pages/search/ArtistSearch.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ArtistSearch = () => {
+  return <>Artist Search</>;
+};
+
+export default ArtistSearch;
