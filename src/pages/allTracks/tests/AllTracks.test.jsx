d49a16a88ac033c95444f8b470631ace2789d30f
Sydney James <sydneyjames7@gmail.com>
2021-02-12 16:43:23 -0800

    trying to fix alltracks test

--- src/pages/allTracks/tests/AllTracks.test.jsx
+++ src/pages/allTracks/tests/AllTracks.test.jsx
@@ -1,16 +1,17 @@
-/* eslint-disable no-undef */
-global.fetch = require("node-fetch");
 import React from "react";
 import { render, screen, waitFor, cleanup } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
-import AllTracks from "./AllTracks";
+import AllTracks from "../AllTracks";
 
 describe("Container AllTracks", () => {
   afterEach(() => cleanup());
 
-  it("displays all tracks for a single album", async() => {
-    render(<MemoryRouter><AllTracks /></MemoryRouter>);
-    const ul = await screen.getByTestId("tracklist");
+  it("displays all tracks for a single album", () => {
+    render(
+      <MemoryRouter>
+        <AllTracks match={{ params: { albumId: "4b112d60-2bb2-4533-8f3a-e3c7977e813c" } }} />
+      </MemoryRouter>);
+    const ul = screen.getByTestId("tracklist");
     return waitFor(() => {
       expect(ul).not.toBeEmptyDOMElement();
     });
